0321bebf3db192e8bc85e619777dc97ae2333bbb
Szymon <sklemcz@gmail.com>
2021-07-18 22:05:10 +0200

    setting default props

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,7 +22,7 @@ var IndecisionApp = function (_React$Component) {
         _this.handlePick = _this.handlePick.bind(_this);
         _this.handleAddOption = _this.handleAddOption.bind(_this);
         _this.state = {
-            options: []
+            options: props.options
         };
         return _this;
     }
@@ -60,13 +60,12 @@ var IndecisionApp = function (_React$Component) {
     }, {
         key: 'render',
         value: function render() {
-            var title = 'Indecision';
             var subtitle = 'Randomize your tasks';
 
             return React.createElement(
                 'div',
                 null,
-                React.createElement(Header, { title: title, subtitle: subtitle }),
+                React.createElement(Header, { subtitle: subtitle }),
                 React.createElement(Action, {
                     hasOptions: this.state.options.length > 0,
                     handlePick: this.handlePick
@@ -85,6 +84,10 @@ var IndecisionApp = function (_React$Component) {
     return IndecisionApp;
 }(React.Component);
 
+IndecisionApp.defaultProps = {
+    options: []
+};
+
 var Header = function Header(props) {
     return React.createElement(
         'div',
@@ -94,7 +97,7 @@ var Header = function Header(props) {
             null,
             props.title
         ),
-        React.createElement(
+        props.subtitle && React.createElement(
             'h2',
             null,
             props.subtitle
@@ -102,18 +105,21 @@ var Header = function Header(props) {
     );
 };
 
-// class Header extends React.Component {
-//    render() {
-//        return(
-//            <div>
-//                <h1>{this.props.title}</h1>
-//                <h2>{this.props.subtitle}</h2>
-//            </div>
-//        )
-//    }
-// }
+Header.defaultProps = {
+    title: 'Indecision'
 
-var Action = function Action(props) {
+    // class Header extends React.Component {
+    //    render() {
+    //        return(
+    //            <div>
+    //                <h1>{this.props.title}</h1>
+    //                <h2>{this.props.subtitle}</h2>
+    //            </div>
+    //        )
+    //    }
+    // }
+
+};var Action = function Action(props) {
     return React.createElement(
         'div',
         null,
