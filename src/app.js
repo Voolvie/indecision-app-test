bc4d70e0ac6bd02e2e6a50f2093729d6c30c1d02
Szymon <sklemcz@gmail.com>
2021-07-23 15:30:10 +0200

    setting up babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-import subtract, {square, add} from './utils.js'
-import isSenior, {isAdult, canDrink} from './person.js'
+// import subtract, {square, add} from './utils.js'
+// import isSenior, {isAdult, canDrink} from './person.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-console.log('app.js is running1')
-// console.log(square(4))
-// console.log(add(22,3))
-// console.log(subtract(10, 2))
-console.log(isAdult(22))
-console.log(canDrink(12))
-console.log(isSenior(74))
+const template = <p>this is jsx from webpack</p>
+ReactDOM.render(template, document.getElementById('app'))
+// console.log('app.js is running1')
+// // console.log(square(4))
+// // console.log(add(22,3))
+// // console.log(subtract(10, 2))
+// console.log(isAdult(22))
+// console.log(canDrink(12))
+// console.log(isSenior(74))
